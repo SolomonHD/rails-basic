529c182e6182b24cd2b211dbd687f272efb7fba5
Solomon Hill <shilli2@localhost.localdomain>
2017-10-26 13:10:34 -0700

    Add touch restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, "git@github.com:SolomonHD/rails-basic"
 set :user, "deploy"
 set :stages, %w(staging)
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids','tmp/cache','tmp/sockets','vendor/bundle', 'public/system')
+set :passenger_restart_with_touch, true
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
