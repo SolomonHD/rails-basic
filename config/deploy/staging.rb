8f56a2b8f382c69eea6b511d3f1b31ebb9af330b
Solomon Hill <shilli2@localhost.localdomain>
2017-11-01 11:14:04 -0700

    change staging rb rails_env to development

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ role :app, %w{deploy@whpihdev1}
 role :web, %w{deploy@whpihdev1}
 role :db, %w{deploy@whpihdev1}, :primary => true
 set :branch, "master"
-set :rails_env, "staging"
+set :rails_env, "development"
 set :deploy_to, "/home/deploy/Apps"
 
 
