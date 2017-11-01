a0df5ece5ddbe747c667e5309f2b6094297398d7
Solomon Hill <shilli2@localhost.localdomain>
2017-11-01 10:59:16 -0700

    Add money controller 3rd time

---
+++ test/system/money_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class MoneyTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit money_url
+  #
+  #   assert_selector "h1", text: "Money"
+  # end
+end
